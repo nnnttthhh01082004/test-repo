be79f6caa4cb9c04caf8bc1b020bd121fa79228b
nnnttthhh01082004 <thanhhungg2k4@gmail.com>
2023-03-22 19:51:56 +0700

    add file html, css, js

---
+++ CONFIG.js
@@ -0,0 +1,12 @@
+const CONFIGDATA = {
+  titleHeader: "Hello sister Vũ Quyên, Let's be friends",
+  descriptionHeader:
+    "Nếu chị thoát ra và không trả lời thì có nghĩa là chị đã ĐỒNG Ý rồi đấy ^^",
+  buttonYes: "Đồng Ý",
+  buttonNo: "Không! Còn lâu nhé.",
+  titleModar: "Cảm ơn chị vì đã đồng ý",
+  descriptionModar:
+    'Em biết là chị sẽ đồng ý làm bạn với em mà ^^ </br> Cảm ơn chị đã đồng ý để cho em được làm bạn với chị, :)),<br>Thank sister for being friends with me!! ^^<br>Love,<br>Hùng-Ngyễn Thanh Hùng',
+  
+  
+};
